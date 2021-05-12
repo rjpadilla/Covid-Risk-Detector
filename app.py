51cd27127ffb08e4bd5369f32d6fd88cbd74f1ba
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-05-11 20:31:39 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+"""
+comorbidity-ml: a webapp to the users the chance of morbidity
+"""
+from flask import Flask, render_template, request, redirect, url_for
+import joblib
+import numpy as np
+import pandas as pd
+import pygal
+
+# Loads the machine learning model
+df = pd.read_csv("data/comorbid.csv")
+lr_cv_model = joblib.load("data/comorbid-trained-model.pkl")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/survey")
+def survey():
+    return render_template("survey.html")
+
+
+@app.route('/predict', methods=['POST'])
+def result():
+    """
+    Function: result
+    Input: none
+    Returns: The result page of the user's prediction
+    """
+    letters = ['age', 'sex', 'smoking', 'healthcare_worker', 'hypertension', 
+               'diabetes', 'dementia', 'cancer', 'copd', 'asthma', 
+               'chd', 'ccd', 'cnd', 'cld', 'ckd']
+    pred_date = [int(request.form[letter]) for letter in letters]
+    arr = np.array([pred_date])
+    pred = lr_cv_model.predict(arr)
+    return render_template('results.html', data=pred)
+
+@app.route('/charts')
+def charts() -> render_template:
+    """
+    Function: charts
+    Input: none
+    Returns: Embedding a pygal chart in the webapp
+    """
+    male_deaths = len(df[(df.death == 1) & (df.sex == 1)])
+    female_deaths = len(df[(df.death == 1) & (df.sex == 0)])
+    # Covid cases by mortality
+    bar_chart = pygal.Bar()
+    bar_chart.title = "Deaths by Gender"
+    bar_chart.add('male', male_deaths)
+    bar_chart.add('female', female_deaths)
+    bar_data = bar_chart.render_data_uri()
+    return render_template('charts.html', bar_data=bar_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ comorbid.py
@@ -0,0 +1,36 @@
+"""
+comorbidity model: uses Logistic Regression to create a prediction model
+"""
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegressionCV
+
+
+df = pd.read_csv("data/comorbid.csv")
+feature_col_names = ['age', 'sex', 'smoking', 'healthcare_worker',
+                     'hypertension', 'diabetes',
+                     'dementia', 'cancer', 'copd', 'asthma', 'chd', 'ccd', 'cnd', 'cld',
+                     'ckd']
+predicted_class_names = ['death']
+
+X = df[feature_col_names].values
+y = df[predicted_class_names].values
+SPLIT_TEST_SIZE = 0.30
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=SPLIT_TEST_SIZE, random_state=42)
+
+lr_cv_model = LogisticRegressionCV(n_jobs=-1, random_state=42, Cs=3, cv=10, refit=False, class_weight="balanced", max_iter=10000)  # set number of jobs to -1 which uses all cores to parallelize
+lr_cv_model.fit(X_train, y_train.ravel())
+
+joblib.dump(lr_cv_model, "data/comorbid-trained-model.pkl")
+
+
+df_predict = pd.read_csv("data/comorbid-predict.csv")
+
+print(df_predict.shape)
+
+X_predict = df_predict
+del X_predict['death']
+
+print(lr_cv_model.predict(X_predict))
