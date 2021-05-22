e88a77a6188906b95d18f18cc2dc55709fd707b6
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-05-21 20:47:29 -0400

    switched to quotes

--- checker.py
+++ checker.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, flash, session, url_for, request
+from flask import redirect, flash, session, url_for, request
 from functools import wraps
 
 def home_logged_in(func):
@@ -17,8 +17,8 @@ def check_logged_in(func):
         if 'logged_in' in session:
             return func(*args, **kwargs)
         else:
-          flash('You need to login first.'.format(request.form.get("title")), "danger")
-          return redirect(url_for('login'))
+            flash("You need to login first.".format(request.form.get("title")), "danger")
+            return redirect(url_for('login'))
     return wrapper
 
 
@@ -26,9 +26,9 @@ def wrong_info(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if (request.form['username'] != 'admin' or request.form['password'] != 'password'):
-            flash('You did not enter the right credentials.'.format(request.form.get("title")), "danger")
+            flash("You did not enter the right credentials.".format(request.form.get("title")), "danger")
             return redirect(url_for('login'))
         else:
-            flash('You successfully logged in!'.format(request.form.get("title")), "success")
+            flash("You successfully logged in!".format(request.form.get("title")), "success")
             return func(*args, **kwargs)
     return wrapper
