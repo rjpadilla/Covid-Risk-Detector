cee3a8c9e07fc028230c78366c6a36116b3d9bfe
rjpadilla <23529006+rjpadilla@users.noreply.github.com>
2021-05-22 19:54:39 -0400

    adding confusion matrix

--- comorbid.py
+++ comorbid.py
@@ -2,9 +2,12 @@
 comorbidity model: uses Logistic Regression to create a prediction model
 """
 import pandas as pd
+import matplotlib.pyplot as plt
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import plot_confusion_matrix
+from sklearn import svm
 
 
 df = pd.read_csv("data/conditions.csv")
@@ -30,5 +33,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=SPLIT_TEST_S
 
 lr_cv_model = LogisticRegressionCV(n_jobs=-1, random_state=42, Cs=3, cv=10, refit=False, class_weight="balanced", max_iter=10000)  # set number of jobs to -1 which uses all cores to parallelize
 lr_cv_model.fit(X_train, y_train.ravel())
+
+disp = plot_confusion_matrix(lr_cv_model, X_test, y_test, cmap=plt.cm.Blues)
+disp.ax_.set_title("Confusion Matrix")
+plt.plot(disp.confusion_matrix)
+plt.savefig('static/images/confusion.png')
 
 joblib.dump(lr_cv_model, "data/comorbid-trained-model.pkl")
